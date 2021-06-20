c80a1ba4bb6009a9bcba1e95425510dc1fbafb6c
paradox921 <59913442+paradox921@users.noreply.github.com>
2021-06-20 22:52:53 +0800

    Add files via upload

---
+++ CourseSelect/wxk.shu.edu.cn-master/jwc/urls.py
@@ -0,0 +1,43 @@
+"""jwc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myWEB import views
+
+urlpatterns = [
+    path('', views.home),  # 首页
+    path('admin/', admin.site.urls),  # admin
+
+    path('calculate/', views.calculate),  # 计算器界面
+    path('calculate-history/', views.calculate_history),  # 计算历史
+
+    path('login_view/', views.login_view),  # 填写完信息后提交登陆验证
+    path('register/', views.register),  # 注册新用户
+    path('logout_view/', views.logout_view),  # 退出登陆
+
+    path('index', views.index),  # 教务系统首页
+
+    path('index/xk/', views.xk),  # 学生选课
+    path('index/tk/', views.tk),  # 学生退课
+    path('index/pingjia/', views.pingjia),  # 学生评价
+    path('index/wode_pingjia/', views.wode_pingjia),  # 学生评价
+    path('index/cjcx/', views.cjcx),  # 学生成绩查询
+    path('index/kccx/', views.kccx),  # 学生课程查询
+
+    path('index/kk/', views.kk),  # 教师开课
+    path('index/qxkk/', views.qxkk),  # 教师取消开课
+    path('index/fbcj/', views.fbcj),  # 教师发布成绩
+]
